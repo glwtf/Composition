c2613ab34a4391dbf8dfb8bca674c46716d20ba3
glebkirill99 <glebkirill99@gmail.com>
2022-11-12 16:48:37 +0300

    end setup clean architecture

---
+++ app/src/main/java/com/example/composition/domain/usecase/GenerateQuestionUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.composition.domain.usecase
+
+import com.example.composition.domain.repository.GameRepository
+
+class GenerateQuestionUseCase(private val repository: GameRepository) {
+
+    operator fun invoke( maxSumValue : Int )
+    = repository.generateQuestion(maxSumValue, COUNT_OF_OPTIONS)
+
+    private companion object {
+        private const val COUNT_OF_OPTIONS = 6
+    }
+}
